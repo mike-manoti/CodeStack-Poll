0eede0201d7b4bfe60331e7ac00312add322b74d
manotim <manotimike@gmail.com>
2023-10-19 15:19:43 +0300

    done restart

--- codestackapp/views.py
+++ codestackapp/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(resource):
-    context ={}
-    render(resource, 'index.html', context)
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
 
-def detail(resource):
-    context ={}
-    render(resource, 'detail.html', context)
+def detail(request):
+    context = {}
+    return render(request, 'detail.html', context)
 
-def result(resource):
-    context ={}
-    render(resource, 'result.html', context)
+def result(request):
+    context = {}
+    return render(request, 'result.html', context)
 
-def signin(resource):
-    context ={}
-    render(resource, 'signin.html', context)
+def signin(request):
+    context = {}
+    return render(request, 'signin.html', context)
 
-def signup(resource):
-    context ={}
-    render(resource, 'signup.html', context)
+def signup(request):
+    context = {}
+    return render(request, 'signup.html', context)
